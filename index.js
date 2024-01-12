08ab1c036e12c77b2907e565c5e2ffcdd9d5236d
Mitchell <mitchell.stockley@gmail.com>
2024-01-12 18:34:25 +0200

    netlify link added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:3000'],
+        origin: ['https://voluble-pegasus-76e234.netlify.app'], //change to netlify?
     }
 })
 
